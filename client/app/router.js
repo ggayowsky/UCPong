a96a94b00b78c07e0188c5d3ad92ed363d6721c6
Christian Natis <cnatis@uncharted.software>
2016-07-07 10:34:42 -0400

    Basic socket.io server, added socket.io to client.

--- client/app/router.js
+++ client/app/router.js
@@ -6,6 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('lobby', { path: '/' });
 });
 
 export default Router;
--- server/app.js
+++ server/app.js
@@ -1,11 +1,61 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+const CONFIG = {
+    PORT: 3000
+};
 
 io.on('connection', function(socket){
     console.log('a user connected');
+    setupSocket(socket);
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+server.listen(CONFIG.PORT, function(){
+    console.log('listening on port 3000');
 });
+
+var users = [];
+var games = [];
+
+// Get list of connected users for lobby
+app.get('/connectedUsers', function(req, res) {
+    console.log(io.sockets.connected);
+    res.json({
+        test: 'hello world!'
+    });
+});
+
+// Change display name of user
+app.post('/user/:id/name', function(req, res) {
+
+});
+
+// Challenge user to a game
+app.post('/user/:id/challenge', function(req, res) {
+
+});
+
+// Complete game
+app.post('/challenge/:id/completed', function(req, res) {
+
+});
+
+var socketEvents = [
+    {
+        eventName: 'user-connected',
+        handler: userConnectedHandler
+    }
+];
+
+function setupSocket(socket) {
+    socketEvents.forEach(ev => {
+        socket.on(ev.eventName, ev.handler);
+    });
+}
+
+function userConnectedHandler(data) {
+    io.emit('user-connected', {
+
+    });
+}
